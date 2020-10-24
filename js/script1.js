907388495d42c6dec2a6aed8a7664d076b23e016
hitinoviy <vovan.korchagin.1999@gmail.com>
2020-10-24 20:14:35 +0300

    inicial commit

--- js/script1.js
+++ js/script1.js
@@ -8,10 +8,50 @@
 // else{
 //     alert('Отмена');
 // }
-let uName = prompt('Как зовут?', 'Никак');
-if(uName==null){
-    alert('Пока!');
+// let uName = prompt('Как зовут?', 'Никак');
+// if(uName==null){
+//     alert('Пока!');
+// }
+// else{
+//     document.write('Привет' + uName);
+// }
+// let a = 1;
+// let b = '1';
+
+// let sr = document.write(b+a);
+// let ir = document.write(a+b);
+// let a = '1';
+// let b = document.write(parseInt(a) + 1);
+
+// let c = '5.1';
+// let d = document.write(parseFloat(c)+ 5);
+
+// let e = '2+2';
+// let f = document.write(eval(e));
+// let a;
+// let b = 1;
+// let c = 2;
+// document.write(a = c<<b);
+let a = 10;
+let b = 5;
+if(!(a==b)){
+    document.write('a!=b');
+} else{
+    document.write('a=b');
 }
-else{
-    document.write('Привет' + uName);
+// let command = prompt('D','');
+
+// if(command== null){
+//     document.write('Ля');
+// } else{
+//     switch(command){ //parseInt как вариант проверки
+//         case 1: alert('Тут 1 ');break;
+//         case 2:alert('Тут 2');break;
+//         default: alert('А тут не то');break;
+//     }
+// }
+let i = 10;
+while(i<19){
+    document.write(i + "<br>");
+    i++;
 }
