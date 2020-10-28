43de50799573982b45ad45e269b7ea49ec72c1e6
hitinoviy <68177024+hitinoviy@users.noreply.github.com>
2020-10-29 00:02:13 +0300

    include commit

--- js/script1.js
+++ js/script1.js
@@ -32,13 +32,13 @@
 // let b = 1;
 // let c = 2;
 // document.write(a = c<<b);
-let a = 10;
-let b = 5;
-if(!(a==b)){
-    document.write('a!=b');
-} else{
-    document.write('a=b');
-}
+// let a = 10;
+// let b = 5;
+// if(!(a==b)){
+//     document.write('a!=b');
+// } else{
+//     document.write('a=b');
+// }
 // let command = prompt('D','');
 
 // if(command== null){
@@ -50,8 +50,57 @@ if(!(a==b)){
 //         default: alert('А тут не то');break;
 //     }
 // }
-let i = 10;
-while(i<19){
-    document.write(i + "<br>");
-    i++;
+// let i = 10;
+// while(i<19){
+//     document.write(i + "<br>");
+//     i++;
+// }
+// for(i=1;i<21;i++){
+//     if(i%2 ==0) continue;
+//     else{document.write(i+'=');}
+// }
+
+// let mass = [1,5,6,8];
+// document.write(mass);
+// mass[7] = 11;
+// document.write(mass);
+let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 10]];
+
+for (let i = 0; i < arr.length; i++) {
+	for (let j = 0; j < arr[i].length; j++) {
+        document.write(arr[i][j] + '<br>');
+        
+	}
 }
+
+let mass = [0,0,0];
+mass[0] = [1,2,3];
+mass[1] = [4,5,6];
+mass[3] = [11,8,9];
+
+let ind = [0, 0];
+let max = mass[0][0];
+
+for(i=0;i<mass.length;i++){
+    for(j=0;j<mass[i].length;j++){
+        if(mass[i][j]>max){
+            max = mass[i][j];
+            ind[0] = i;
+            ind[1] = j;
+        }
+    }
+}
+document.write(max);
+
+
+// let mass =[[1,2,3,7],[4,5,6]];
+// let max = mass[0][0];
+
+// for(let i=0;i<mass.length;i++){
+//     for(let j=0;j<mass[i].length;j++){
+//         if(mass[i][j]>max){
+//             max = mass[i][j];
+//         }
+//     }
+// }
+// document.write('<h1>' + max + '</h1>');
